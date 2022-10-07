853594b655cf6889c14e37af56035e0ed33b262b
S <30459885+tacticstactics@users.noreply.github.com>
2022-10-07 17:35:33 +0900

    1

--- PythonApplication1/PythonApplication1/Thinlayer_def.py
+++ PythonApplication1/PythonApplication1/Thinlayer_def.py
@@ -4,13 +4,15 @@ import numpy as np
 import math
 
 
-def dielectric(wl, n1, n2, th1, th2, Ein=np.array([[1],[0]])):
+def dielectric(wl, n1, n2, th1, th2, Ein):
+
+    D21 = (1/(2*np.sqrt(n1*n2)))* np.array([[n1+n2, n2-n1],[n2-n1, n1+n2]]);
+    D12 = (1/(2*np.sqrt(n1*n2)))* np.array([[n1+n2, n1-n2],[n1-n2, n1+n2]]);
+
 
     T1 = np.array([[np.exp(-1j*n1*th1*2*math.pi/wl), 0], [0, np.exp(1j*n1*th1*2*math.pi/wl)]]);
     T2 = np.array([[np.exp(-1j*n2*th2*2*math.pi/wl), 0], [0, np.exp(1j*n2*th2*2*math.pi/wl)]]);
 
-    D21 = (1/(2*np.sqrt(n1*n2)))* np.array([[n1+n2, n2-n1],[n2-n1, n1+n2]]);
-    D12 = (1/(2*np.sqrt(n1*n2)))* np.array([[n1+n2, n1-n2],[n1-n2, n1+n2]]);
     
     E1 = np.dot(D21,Ein)
     E2 = np.dot(T1,E1)
