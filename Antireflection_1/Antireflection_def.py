fcd29886daa06d2a0e314bf13abe8643bc71671d
S <30459885+tacticstactics@users.noreply.github.com>
2022-10-18 11:26:55 +0900

    1

--- Antireflection_1/Antireflection_def.py
+++ Antireflection_1/Antireflection_def.py
@@ -8,7 +8,11 @@ def transfermatrix(wl, n1, n2, th1, th2, Ein):
 m11 = Morig(1,1);
      m12 = Morig(1,2);
      m21 = Morig(2,1);
-     m22 = Morig(2,2);     
-     t = (2*n0) ./ ((m11 + m12*ns)*n0 + (m21 + m22*ns));
+     m22 = Morig(2,2);
+     
+     t = (2*n0) ./ ((m11 + m12*ns)*n0 + (m21 + m22*ns))
+
+
+
 
 
