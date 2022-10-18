173bdc6e8fd24f75b7bcff49bbafcb4adc05d1de
S <30459885+tacticstactics@users.noreply.github.com>
2022-10-18 17:17:00 +0900

    1

--- Antireflection_1/Antireflection_def.py
+++ Antireflection_1/Antireflection_def.py
@@ -3,14 +3,32 @@
 import numpy as np
 import math
 
-def transfermatrix(wl, n1, n2, th1, th2, Ein):
 
-m11 = Morig(1,1);
-     m12 = Morig(1,2);
-     m21 = Morig(2,1);
-     m22 = Morig(2,2);
-     
-     t = (2*n0) ./ ((m11 + m12*ns)*n0 + (m21 + m22*ns))
+
+def transfermatrix(wl, n_index, th1):
+
+    nL = 1.463 + 0.003827/(wl**2) + 0.000/(wl**4)
+    nH = 2.1305 + 0.018499/(wl**2) + 0.00199850/(wl**4)
+    #nQuartz = 1.6553 + 0.0086444/(wl.^2) + 0.00081178./(wl.^4)
+    #nLN = sqrt(2.23413 + ((2.68312-wl.^2)/(wl.^2-0.04481)) + ((2.59121.* wl)/(wl - 109.776)));
+ 
+    #substrate: quartz
+  
+    match n_index:
+
+        case 1:
+            n_index = nL
+        case 2:
+            n_index = nH
+
+
+    sigma = 2 * math.pi *n_index*th1/wl
+
+   	#Mjj = np.array([[math.cos(sigma), ],[np.exp(1j*n1 * math.sin(sigma)), math.cos(sigma)]])
+   	
+    TM_intermedate = np.array([[math.cos(sigma),np.exp(1j)*math.sin(sigma)/n_index],[np.exp(1j)*math.sin(sigma)/n_index,math.cos(sigma)]])
+
+    return TM_intermedate
 
 
 
--- Antireflection_1/Antireflection_main.py
+++ Antireflection_1/Antireflection_main.py
@@ -1,6 +1,7 @@
 #Antireflection_main.py
 
 import math
+from tkinter import NS
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,134 +18,73 @@ import Antireflection_def
 startwl = 1.200 # [um]
 stepwl = 0.002 # [um]
 
-nn = 512 # number of sampling point
+mm = 512 # number of sampling point
 
+layer1 = np.array([1, 2, 1, 2])
+
+thkpara = np.array([0.181924, 0.181924, 0.181924, 0.181924])
+
+len_layer1 = len(layer1)
+print('Length of L1 = ')
+print(len_layer1)
+
+wlcol= np.zeros(mm,1)
+P1dBcol=np.zeros(mm,1)
+P2dBcol=np.zeros(mm,1)
  
- 
- %__________________________________________________              
- n0 = 1.000;
 
-
- layer1 = np.array([0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866, 0.264866])
-
- L2 = np.array([0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924, 0.181924])
-
- 
- len_layer1 = len(layer1)
- print('Length of L1 = ')
- print(len_layer1)
- 
-  
-  global wlcol 
-  global dBTcol 
-  global dBRcol 
-
-  wlcol= np.zeros(mm,1);
-  dBTcol=np.zeros(mm,1);
-  dBRcol=np.zeros(mm,1);
-
- Mjj = [];
- 
- for ii = 1:mm:
+for ii in range(mm):   
 
     wl = startwl + stepwl *ii
+    wlcol[ii,1] = wl
 
-    wlcol(ii,1) = wl
+    TMin = np.array([[1,0],[0,1]])
     
-     nL = 1.463 + 0.003827/(wl.^2) + 0.000./(wl.^4);
-     nH = 2.1305 + 0.018499/(wl.^2) + 0.00199850./(wl.^4);
-	nQuartz = 1.6553 + 0.0086444/(wl.^2) + 0.00081178./(wl.^4);
-	#nLN = sqrt(2.23413 + ((2.68312-wl.^2)/(wl.^2-0.04481)) + ((2.59121.* wl)/(wl - 109.776)));
- 
-  #substrate: quartz
-  ns = nQuartz;
- 
-  
- for kk = 1:ll:
-       
-         TM_intermedate = Antireflection_def.transfermatrix(wl, n1, n2, th1, th2, TMin)
-
-       #print(jj)
-
-        th1 = L1[jj]
-        th2 = L2[jj]           
-
-        TM_intermedate = Thinlayer_def.dielectric(wl, n1, n2, th1, th2, TMin)
-        TMin = TM_intermedate
-        #print(E_intermedate)
+    n0 = 1
+    ns = 1.463 + 0.003827/(wl**2) + 0.000/(wl**4)
     
-   TMout = TM_intermedate
-    
-   Eout2 = np.dot(TMout, Ein)
 
-   Eout2_x = Eout2[0,0]
-   Eout2_y = Eout2[1,0]
-   
-   Pow1 = np.abs(Eout2_x)**2
-   Pow2 = np.abs(Eout2_y)**2  
+    for kk in range(len_layer1):
 
-   P1dBcol[ii] = -10*np.log(Pow1)
-   P2dBcol[ii] = 10*np.log(Pow2)
+        n_index = layer1[kk]
+        thickness1 = thkpara[kk]
 
+        TM_intermediate = Antireflection_def.transfermatrix(wl, n_index, thickness1, TMin)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-       	if layer(kk) == 0
-	        n = nL; #% L 
-     	   
-	     elseif   layer(kk) == 1
-	        n = nH; #% H 
-
-	      elseif   layer(kk) == 'A'
-	        n = n0; #% AH 
-   		end
-
-     h(kk) =  (centerwl ./ n) .* thkpara(kk);   # thickness is quarter !!!
-     sigma = 2 * math.p i *n*h(kk) / wl;   
+        TMin = TM_intermediate
+        #inner loop ended
         
-   	Mjj = [math.cos(sigma), 1j*math.sin(sigma) / n;
-      1j*n * math.sin(sigma), math.cos(sigma)]; 
-              
-              if kk ==1
-               Morig = eye(2,2);
-              end;
-              
-       M = Morig * Mjj;       
-       Morig = M;
-       
-	end
-            
-     m11 = Morig(1,1);
-     m12 = Morig(1,2);
-     m21 = Morig(2,1);
-     m22 = Morig(2,2);     
-     t = (2*n0) ./ ((m11 + m12*ns)*n0 + (m21 + m22*ns));
-  	       
-  T = t*conj(t) * (ns/n0);
+    TMout = TM_intermediate
 
-  dBTcol(ii,1) = 10*log10(T);
-  
-  B = m11 + m12*ns;
-  C = m21 + m22 * ns;
-  r = (n0*B - C) ./ (n0 * B + C);
-  R = r * conj(r);
-  
-  dBRcol(ii,1) = 10*log10(R)
+    print(TMout)
+
+    #     m11 = TMout[0,0]
+    #     m12 = TMout[0,1]
+    #     m21 = TMout[1,0]
+    #     m22 = TMout[1,1]
+
+     
+
+
+       #B = m11 + m12*ns
+       #C = m21 + m22 * ns
+       
+       #er = (n0*B - C) / (n0 * B + C)     
+       #et = (2*n0) / ((m11 + m12*ns)*n0 + (m21 + m22*ns))
+       
+       er = 1
+       et = 0
+
+       Pow1 = np.abs(er)**2
+       Pow2 = np.abs(et)**2 * ns/n0
+       
+       P1dBcol[ii] = -10*np.log(Pow1)
+       P2dBcol[ii] = 10*np.log(Pow2)
+
+    #outer loop ended
 
 
 fig = plt.figure(figsize = (8,4), facecolor='lightblue')
-
 ax1 = fig.add_subplot(2, 1, 1)
 ax2 = fig.add_subplot(2, 1, 2)
 
